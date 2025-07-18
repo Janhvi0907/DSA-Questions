6024cf5d19be949c84f60fbe0ea0869cebdda2bb
Janhvi Singh <singhhardik020@gmail.com>
2025-07-18 22:02:37 +0530

    Initial commit

---
+++ MissionDSA/hackerrank/acmteam.py
@@ -0,0 +1,11 @@
+def acmTeam(topic):
+    #Main function to find the maximum number of topics known by a team
+    teams = list(itertools.combinations(topic, 2))
+    subjects = []
+    for team in teams:
+        known = 0
+        for topic in range(len(team[0])):
+            if team[0][topic] == "1" or team[1][topic] == "1":
+                known += 1
+        subjects.append(known)  
+    return [max(subjects), subjects.count(max(subjects))]
---
+++ README.md
@@ -0,0 +1,2 @@
+# MissionDSA
+This Repo Contains code of all the problems which are discussed in the lecture .
